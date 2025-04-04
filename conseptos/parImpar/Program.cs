373beb8009479940ab41a815108a50c34b2bca1f
norbeyh <marionorbeyhenao8@gmail.com>
2025-04-04 10:56:58 -0500

    par e impar

--- conseptos/parImpar/Program.cs
+++ conseptos/parImpar/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Introduce un número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+// Comprobar si el número es par o impar
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El número {0} es par.", numero);
+}
+else
+{
+    Console.WriteLine("El número {0} es impar.", numero);
+}
+   
